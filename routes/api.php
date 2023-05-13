43c7cdc49ea2bdb469e09c3df51cc1b5b9bc74e8
adcavalcant <aderc19@gmail.com>
2023-05-12 20:00:39 -0400

    último commit

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Hash;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,18 +19,31 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('/series', 'App\Http\Controllers\Api\SeriesController');
-Route::get('/series/{series}/seasons', function(\App\Models\Series $series){
-    return $series->seasons;
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('/series', 'App\Http\Controllers\Api\SeriesController');
+    Route::get('/series/{series}/seasons', function (\App\Models\Series $series) {
+        return $series->seasons;
+    });
+
+    Route::get('/series/{series}/episodes', function (\App\Models\Series $series) {
+        return $series->episodes;
+    });
+
+    Route::patch('/episodes/{episode}', function (\App\Models\Episode $episode, Request $request) {
+        $episode->watched = $request->watched;
+        $episode->save();
+        return $episode;
+    });
+
 });
 
-Route::get('/series/{series}/episodes', function (\App\Models\Series $series) {
-    return $series->episodes;
-});
 
-Route::patch('/episodes/{episode}', function(\App\Models\Episode $episode, Request $request){
-    $episode->watched = $request->watched;
-    $episode->save();
-    return $episode;
+Route::post('/login', function (Request $request) {
+    $credentials = $request->only(['email', 'password']);
+    if (\Illuminate\Support\Facades\Auth::attempt($credentials) === false) {
+        return response()->json('Unauthorized', 401);
+    }
+    $user = \Illuminate\Support\Facades\Auth::user();
+    $token = $user->createToken('token');
+    return response()->json($token->plainTextToken);
 });
-
